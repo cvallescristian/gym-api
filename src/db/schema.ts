0ba2d983f570eb31b903b32defdc20f1bb55248d
Cristian Valles Pereira <valles.cristian1992@gmail.com>
2024-08-20 16:10:55 +1200

    feat: add image column to exercise schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,5 +3,6 @@ import { pgTable, serial, text } from 'drizzle-orm/pg-core';
 export const exercises = pgTable('exercises', {
 	id: serial('id').primaryKey(),
 	name: text('name'),
-	description: text('description'),
+	image: text('image'),
+	description: text('description')
 });

0b35ab5409b520651b15524addff8673ee5c50bd
Cristian Valles Pereira <valles.cristian1992@gmail.com>
2024-09-10 13:47:34 +1200

    feat: added CORS

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,26 @@ import { Hono } from 'hono';
 import { saveImage } from './services/cloudinary/utils';
 import exerciseApp from './services/api/exercises/exercises.api';
 import trainingApp from './services/api/trainings/trainings.api';
+import { cors } from 'hono/cors';
 
 export type Env = {
 	DATABASE_URL: string;
 	CLOUDINARY_API_KEY: string;
 	CLOUDINARY_API_SECRET: string;
 	CLOUDINARY_CLOUD_NAME: string;
+	CLOUDINARY_FOLDER_NAME: string;
 };
 
 const api = new Hono<{ Bindings: Env }>();
 
+// Cors
+const app = new Hono();
+
+api.use('/api/*', cors())
+
 // Routes
-api.route("/api/exercises", exerciseApp);
-api.route("/api/trainings", trainingApp);
+api.route('/api/exercises', exerciseApp);
+api.route('/api/trainings', trainingApp);
 
 // General routes
 api.get('/', async (c) => {

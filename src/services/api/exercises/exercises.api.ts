b7353c0843b5365de775e45047c564c5d39b2c92
Cristian Valles Pereira <valles.cristian1992@gmail.com>
2024-10-01 20:05:28 +1300

    feat: [Exercise] delete create API, added integration API

--- src/services/api/exercises/exercises.api.ts
+++ src/services/api/exercises/exercises.api.ts
@@ -1,10 +1,7 @@
 import { Hono } from 'hono';
-import { saveImage } from '../../../services/cloudinary/utils';
-import getListExercises from '../../../services/drizzle/exercises/list';
-import { NewExercise } from '../../../db/schema';
-import createExercise from '../../../services/drizzle/exercises/create';
 import { Env } from '../../../index';
 import { getExercises } from '../../muscleWiki/exercises';
+import integration from '../../drizzle/exercises/integration';
 
 const exerciseApp = new Hono<{ Bindings: Env }>();
 
@@ -19,30 +16,9 @@ exerciseApp.get('/', async (c) => {
 	return c.json(muscleWikiExercises);
 });
 
-exerciseApp.post('/create', async (c) => {
-	const form = await c.req.formData();
-	const file = form.get('exercise_photo') as File;
-	const name = form.get('name') as string;
-	const link = form.get('link') as string;
-	const muscleId = parseInt(form.get('muscle_id') as string);
-	const description = form.get('description') as string;
-
-	if (!file) {
-		return c.json({ message: 'No image provided' }, 400);
-	}
-
-	const folderName = 'exercises';
-	const savedFilePath = await saveImage({file, env: c.env, folder: folderName});
-
-	const newExercise: NewExercise = {
-		name,
-		description,
-		muscleId,
-		link,
-		image: savedFilePath.secure_url,
-	};
-	const exercise = await createExercise(c.env, newExercise);
-	return c.json(exercise);
+exerciseApp.post('/integration', async (c) => {
+	const response = await integration(c.env);
+	return c.json(response);
 });
 
 export default exerciseApp;

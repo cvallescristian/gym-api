561bf5e670638b9d75bc0cb17cc62e649c86d58c
Cristian Valles Pereira <valles.cristian1992@gmail.com>
2024-10-01 19:13:24 +1300

    feat: [Muscles] delete create api, added integration API, use DB again for list

--- src/services/api/muscles/muscles.api.ts
+++ src/services/api/muscles/muscles.api.ts
@@ -5,37 +5,20 @@ import { NewMuscle } from '../../../db/schema';
 import getListMuscles from '../../drizzle/muscles/list';
 import createMuscle from '../../drizzle/muscles/create';
 import { getMuscles } from '../../muscleWiki/muscles';
+import integration from '../../drizzle/muscles/integration';
 
 
 const muscleApp = new Hono<{ Bindings: Env }>();
 
 muscleApp.get('/', async (c) => {
-	// const listMuscles = await getListMuscles(c.env);
-	const muscleWikiMuscles = await getMuscles();
+	 const listMuscles = await getListMuscles(c.env);
 	
-	return c.json(muscleWikiMuscles);
+	return c.json(listMuscles);
 });
 
-muscleApp.post('/create', async (c) => {
-	const form = await c.req.formData();
-	const file = form.get('muscle_photo') as File;
-	const name = form.get('name') as string;
-
-  console.log({form, name, file})
-
-	if (!file) {
-		return c.json({ message: 'No image provided' }, 400);
-	}
-
-  const folderName = 'muscles'
-	const savedFilePath = await saveImage({file, env: c.env, folder: folderName});
-
-	const newMuscle: NewMuscle = {
-		name,
-		image: savedFilePath.secure_url,
-	};
-	const muscle= await createMuscle(c.env, newMuscle);
-	return c.json(muscle);
+muscleApp.post('/integration', async (c) => {
+	const response = await integration(c.env);
+	return c.json(response);
 });
 
 export default muscleApp;

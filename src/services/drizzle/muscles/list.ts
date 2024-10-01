eec98a105ceb1eb6c1509ae19d6a5a8c016c5edf
Cristian Valles Pereira <valles.cristian1992@gmail.com>
2024-10-01 19:12:04 +1300

    feat: adding filters to list of muscles

--- src/services/drizzle/muscles/list.ts
+++ src/services/drizzle/muscles/list.ts
@@ -1,14 +1,23 @@
-import postgres from "postgres";
-import { Env } from "../../..";
-import { drizzle } from "drizzle-orm/postgres-js";
-import { muscles } from "../../../db/schema";
+import postgres from 'postgres';
+import { Env } from '../../..';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import { muscles } from '../../../db/schema';
+import { eq, ilike, } from 'drizzle-orm';
+
+const withFiltersQuery = (db: any, params: { wikiId?: number, name?: string }) => {
+	if (params?.wikiId) db = db.where(eq(muscles.wikiId, params.wikiId));
+  if (params?.name) db = db.where(ilike(muscles.name, `%${params.name}%`)); 
+
+	return db;
+};
 
 const getListMuscles = async (env: Env) => {
-  const queryClient = postgres(env.DATABASE_URL!);
+	const queryClient = postgres(env.DATABASE_URL!);
 	const db = drizzle(queryClient);
-	const AllMuscles = await db.select().from(muscles);
+	const baseQuery = db.select().from(muscles);
+	const AllMuscles = await withFiltersQuery(baseQuery, {});
 
-  return AllMuscles;
-}
+	return AllMuscles;
+};
 
 export default getListMuscles;
